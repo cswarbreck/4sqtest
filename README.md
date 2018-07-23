04ec268570391ce163fc625101c0883a6dc56dce
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-07-23 15:39:02 +0100

    final draft finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 4sqtest
+
+A simple jQuery project using the Foursquare API to return a list of recommended venues.
--- app/js/index.js
+++ app/js/index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         var enteredSearch = $('#enterSearch').val();
         var placeSearch = $('#placeSearch').val();
 
-        var url = "https://api.foursquare.com/v2/venues/search?v=20180715&intent=browse&radius=1000&near="+placeSearch+"&limit=10&query="+enteredSearch+"&client_id=REZMP55CN0LQT4RIP5UPQ32EMXOUD0RMVV5KNKZL2NVYRQZY&client_secret=UJX5I0FH51FHWLLEW4Y2FSYYCVG0SCGLSGBGFCW5P3ZURQ1I&v=20180718";
+        var url = "https://api.foursquare.com/v2/venues/search?v=20180715&intent=browse&radius=500&near="+placeSearch+"&limit=10&query="+enteredSearch+"&client_id=REZMP55CN0LQT4RIP5UPQ32EMXOUD0RMVV5KNKZL2NVYRQZY&client_secret=UJX5I0FH51FHWLLEW4Y2FSYYCVG0SCGLSGBGFCW5P3ZURQ1I&v=20180718";
         $.ajax({
           url: url, 
           dataType: 'json',
@@ -16,14 +16,13 @@ $(document).ready(function(){
           success: function(data){
             $('#searchResults').html('');  
             var venues = data.response.venues;
+
             $.each(venues, function(i,venue){
               $('ul').append(venue.name + '<br />');
+              $('ul').append(venue.location.formattedAddress + '<br />' + '<br />');
             });
             
             $('#conductSearch').val('');        
-  
-            // Reverse the order of the unordered list items.
-            // Credit to Stack Overflow: http://stackoverflow.com/questions/5347839/jquery-reversing-the-order-of-child-elements
             var list = $('ul');
             var listItems = list.children('li');
             list.append(listItems.get().reverse());
@@ -34,12 +33,15 @@ $(document).ready(function(){
           error:function(errorCommunication){
             alert("Did you complete both fields?");
           }
-
-        }); //end of AJAX
-        
+        }); //end of AJAX  
     });// end of conduct search
 
     $('#enterSearch').keypress(function(typing){
+        if (typing.which===13) {
+          $('#conductSearch').click();
+        }
+      });
+      $('#placeSearch').keypress(function(typing){
         if (typing.which===13) {
           $('#conductSearch').click();
         }
